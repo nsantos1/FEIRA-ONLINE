cc307aa36881a23cafe7d8ae2da252cec323b22f
nsantos1 <devnicolassantos@gmail.com>
2025-11-18 23:32:40 -0300

    Alterando Pitch Video

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -93,15 +93,7 @@ export default function Index() {
           <h2>
             <i className="fa-brands fa-youtube"></i> Mais sobre a Feira Online
           </h2>
-          <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/swLgt21uTB0?si=4ywTKDZc55U4bJT8"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/U7lqywa45Ac?si=dl4IlMTI1K78VtP-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
 
         <div className="localizacao">
